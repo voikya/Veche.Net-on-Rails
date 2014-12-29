d9d983e22cb51105a099abef832bf4b914281cbd
Martin Posthumus <mposthumus@flatworldknowledge.com>
2014-12-28 20:45:01 -0500

    Cleanup of contact page/links

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
-  default :to => "republicofnovegrad@gmail.com"
+  default :to => "republicofnovegrad@gmail.com", :from => "republicofnovegrad@gmail.com"
   layout 'mailer'
 
   def contact(subject, message, email)
     @message = message
-    mail(:from => email, :subject => subject)
+    mail(:reply_to => email, :subject => subject)
   end
 end
