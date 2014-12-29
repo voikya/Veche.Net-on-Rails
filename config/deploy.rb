cb6832a8ce6bd51766d7f683a7f188529fea293c
Martin Posthumus <mposthumus@flatworldknowledge.com>
2014-12-28 19:33:18 -0500

    Add production environment file to capistrano linked files list

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :log_level, :debug
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w(config/database.yml config/initializers/secret_token.rb)
+set :linked_files, %w(config/database.yml config/initializers/secret_token.rb config/environments/production.rb)
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
