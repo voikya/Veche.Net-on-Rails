5a0edada78488fd3d7b73ee203bb16b0324bfd2a
Martin Posthumus <mposthumus@flatworldknowledge.com>
2016-07-08 23:05:09 -0400

    Add Tunisian Adjective Morphology grammar page
    Adds the Adjectives page to the Tunisian grammar.

--- app/controllers/grammars/tunisian_controller.rb
+++ app/controllers/grammars/tunisian_controller.rb
@@ -4,12 +4,13 @@ module Grammars
     before_filter :set_context
 
     @@pages = [
-      {:index => 1, :doc => 'background',  :title => 'Background'        },
-      {:index => 2, :doc => 'phonology',   :title => 'Phonology'         },
-      {:index => 3, :doc => 'orthography', :title => 'Orthography'       },
-      {:index => 4, :doc => 'verbs',       :title => 'Verbal Morphology' },
-      {:index => 5, :doc => 'nouns',       :title => 'Nominal Morphology'},
-      {:index => 6, :doc => 'pronouns',    :title => 'Pronouns'          }
+      {:index => 1, :doc => 'background',  :title => 'Background'              },
+      {:index => 2, :doc => 'phonology',   :title => 'Phonology'               },
+      {:index => 3, :doc => 'orthography', :title => 'Orthography'             },
+      {:index => 4, :doc => 'verbs',       :title => 'Verbal Morphology'       },
+      {:index => 5, :doc => 'nouns',       :title => 'Nominal Morphology'      },
+      {:index => 6, :doc => 'pronouns',    :title => 'Pronouns'                },
+      {:index => 7, :doc => 'adjectives',  :title => 'Adjectives and Numerals' }
     ]
 
     def index
